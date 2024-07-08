0da6bae013d0dc19e3062713cebd79959bf4e5a8
hanzala267 <mhanzala267@gmail.com>
2024-07-08 19:15:32 +0500

    9

--- src/app/client/Services/page.jsx
+++ src/app/client/Services/page.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar";
-import OngoingService from "../components/OngoingServices";
+import OngoingService from "../components/OngoingService"; // Changed from OngoingServices to OngoingService
 import UpcomingService from "../components/UpcomingService";
 import ClosedService from "../components/ClosedService";
 import { withRoleProtection } from "../../../components/withRoleProtection";
