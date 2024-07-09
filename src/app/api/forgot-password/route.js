4964a965271a057249eae3652227aa145d750d37
hanzala267 <mhanzala267@gmail.com>
2024-07-09 10:38:21 +0500

    30

--- src/app/api/forgot-password/route.js
+++ src/app/api/forgot-password/route.js
@@ -5,12 +5,9 @@ import { v4 as uuidv4 } from "uuid";
 export async function POST(req) {
   const { email } = await req.json();
 
-  console.log("Received email:", email); // Log received email
-
   try {
     const user = await prisma.user.findUnique({ where: { email } });
     if (!user) {
-      console.log("User not found for email:", email); // Log user not found
       return new Response(JSON.stringify({ error: "User not found" }), {
         status: 404,
         headers: { "Content-Type": "application/json" },
@@ -28,15 +25,13 @@ export async function POST(req) {
       },
     });
 
-    console.log("Updated user with reset token:", resetToken); // Log token creation
-
     const transporter = createTransport({
       service: "gmail",
       secure: true,
       port: 465,
       auth: {
-        user: process.env.EMAIL_FROM,
-        pass: process.env.EMAIL_PASSWORD,
+        user: "mhanzala267@gmail.com",
+        pass: "byjuetuwkqkpkiru",
       },
     });
 
@@ -52,16 +47,15 @@ export async function POST(req) {
       `,
     };
 
-    await new Promise((resolve, reject) => {
-      transporter.sendMail(mailOptions, (error, emailResponse) => {
-        if (error) {
-          console.error("Error sending email:", error); // Log email sending error
-          reject(new Error("Something went wrong"));
-        } else {
-          console.log("Email sent successfully:", emailResponse); // Log successful email
-          resolve();
-        }
-      });
+    await transporter.sendMail(mailOptions, (error, emailResponse) => {
+      if (error) {
+        console.error("Error sending email:", error);
+        return new Response(JSON.stringify({ error: "Something went wrong" }), {
+          status: 500,
+          headers: { "Content-Type": "application/json" },
+        });
+      }
+      console.log("Email sent successfully:", emailResponse);
     });
 
     return new Response(
@@ -72,7 +66,7 @@ export async function POST(req) {
       }
     );
   } catch (error) {
-    console.error("Error in password reset process:", error); // Log overall process error
+    console.error("Error sending email:", error);
     return new Response(JSON.stringify({ error: "Something went wrong" }), {
       status: 500,
       headers: { "Content-Type": "application/json" },
